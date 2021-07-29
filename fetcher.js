8e00cf6b0e4a54784c7e8499a1bf7db7fbac51af
cupokyle <kyleforsythcomedy@gmail.com>
2021-07-29 01:25:53 +0000

    Finished assignment and stretch 1 & 3

--- fetcher.js
+++ fetcher.js
@@ -1,8 +1,32 @@
 const net = require('net');
 const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const localFilePath = process.argv.slice(2)[1];
 const firstURL = process.argv.slice(2)[0];
+
+// const pathChecker = function(fp) {
+//   let solution = "";
+//   let reversed = fp.split("").reverse().join("");
+//   let startPath = false;
+//   for (let i = 0; i < reversed.length; i++) {
+//     if (startPath === true) {
+//       solution += reversed[i];
+//     }
+//     if (reversed[i] === "/") {
+//       startPath = true;
+//     }
+//   }
+//   solution = solution.split("").reverse().join("");
+//   return solution;
+// };
+
+// let pathCheck = pathChecker(localFilePath);
 
 const urlRepair = function(url) {
   let myURL = "";
@@ -23,33 +47,80 @@ const urlRepair = function(url) {
 
 let myURL = urlRepair(firstURL);
 
-console.log(myURL);
+// if ((fs.existsSync(pathCheck) && fs.lstatSync(localFilePath).isDirectory()) === false) {
+//   console.log("Is this a joke to you?!\nPlease provide a valid directory or file path!");
+//   process.exit();
+// }
 
-const conn = net.createConnection({
-  host: myURL,
-  port: 80
-});
+function isValidURL(string) {
+  let res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  return (res !== null);
+}
 
-conn.setEncoding('UTF8');
+if (isValidURL(myURL)) {
 
-conn.on('connect', () => {
-  console.log('Connected to server');
-
-  conn.write(`GET / HTTP/1.1\r\n`);
-  conn.write(`Host: ${myURL}\r\n`);
-  conn.write('\r\n');
-});
-console.log("Connecting to server...");
-
-conn.on('data', (data) => {
-  const content = data;
-  fs.writeFile(`${localFilePath}`, content, err => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(`Downloaded and saved ${content.length} bytes to ${localFilePath}`);
+  const conn = net.createConnection({
+    host: myURL,
+    port: 80
   });
-  conn.end();
-});
 
+  conn.setEncoding('UTF8');
+
+  conn.on('connect', () => {
+    console.log('Connection Established');
+
+    conn.write(`GET / HTTP/1.1\r\n`);
+    conn.write(`Host: ${myURL}\r\n`);
+    conn.write('\r\n');
+  });
+  console.log("Connecting to server...");
+
+  setTimeout(() => {
+    fs.access(localFilePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        rl.question(`File doesn't exist yet. \nWant to create a new file? (Y/N): `, (answer) => {
+          if (answer[0] === "Y" || answer[0] === 'y') {
+            conn.on('data', (data) => {
+              const content = data;
+              fs.writeFile(`${localFilePath}`, content, err => {
+                if (err) {
+                  console.log(err);
+                  return;
+                }
+                console.log(`Downloaded and saved ${content.length} bytes to ${localFilePath}`);
+              });
+              conn.end();
+              rl.close();
+            });
+          } else {
+            conn.end();
+            rl.close();
+          }
+        });
+      } else {
+        rl.question(`File Already Exists. \nWant to overwrite this file? (Y/N): `, (answer) => {
+          if (answer[0] === "Y" || answer[0] === 'y') {
+            conn.on('data', (data) => {
+              const content = data;
+              fs.writeFile(`${localFilePath}`, content, err => {
+                if (err) {
+                  console.log(err);
+                  return;
+                }
+                console.log(`Downloaded and saved ${content.length} bytes to ${localFilePath}`);
+              });
+              conn.end();
+              rl.close();
+            });
+          } else {
+            conn.end();
+            rl.close();
+          }
+        });
+      }
+    });
+  }, 100);
+} else {
+  console.log("Is this a joke to you?!\nPlease provide a valid URL!");
+  process.exit();
+}
